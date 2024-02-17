e36f1b61bc3fc0b90101a2f98b782f670eae337d
Krigoxar <94596936+Krigoxar@users.noreply.github.com>
2024-02-17 23:40:40 +0300

    Minor twix https://static.wikia.nocookie.net/edopedia/images/6/67/%D0%A2%D0%B2%D0%B8%D0%BA%D1%81.....JPG/revision/latest?cb=20170603174947&path-prefix=ru

--- Lab1/pingme/src/main/java/com/pingme/pingme/presentors/PingPresentor.java
+++ Lab1/pingme/src/main/java/com/pingme/pingme/presentors/PingPresentor.java
@@ -3,7 +3,6 @@ package com.pingme.pingme.presentors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
 
 import com.pingme.pingme.dtos.ExtServerRespond;
 import com.pingme.pingme.dtos.UrlDTO;
@@ -13,7 +12,7 @@ import com.pingme.pingme.services.PingService;
 public class PingPresentor {
 
 	@GetMapping("/ping")
-	public ExtServerRespond ping(@RequestParam(value = "url") String url) throws IOException {
+	public ExtServerRespond ping(@RequestParam(value = "url") String url) {
 		
 			return PingService.pingExternalServer(new UrlDTO(url));
 	}
--- Lab1/pingme/src/test/java/com/pingme/pingme/PingmeApplicationTests.java
+++ Lab1/pingme/src/test/java/com/pingme/pingme/PingmeApplicationTests.java
@@ -1,13 +1,19 @@
 package com.pingme.pingme;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pingme.pingme.dtos.UrlDTO;
+import com.pingme.pingme.services.PingService;
 
 @SpringBootTest
 class PingmeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, PingService.pingExternalServer(new UrlDTO("google.com")).IsResponding());
 	}
 
 }
